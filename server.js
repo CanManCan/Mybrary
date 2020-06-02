c95f56df57f4ffbb2cdcd7556efb4dbd4da27d1a
CanManCan <jminiatt@gmail.com>
2020-06-02 20:06:48 +0300

    Books Create/Index/New

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').load()
+    require('dotenv').config()
   }
 
 const express = require('express');
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -27,6 +28,7 @@ db.on('error', error => console.log(error));
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter);
-app.use('/authors', authorRouter)
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT  || 3000);
